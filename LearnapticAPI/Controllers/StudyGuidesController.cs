9eba7a82cd7f45ff51b64b589536374fa422193f
hmeekins <151791982+hmeekins@users.noreply.github.com>
2026-06-21 17:15:29 -0400

    Created basic test controller

---
+++ LearnapticAPI/Controllers/StudyGuidesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learnaptic.Api.Controllers
+{
+    [ApiController]
+    [Route("api/study-guides")]
+    public class StudyGuidesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Study guides endpoint is working!";
+        }
+    }
+}
