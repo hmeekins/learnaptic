ed8a3ff35ab3ae2e439b2ccbc413d61210f4d698
hmeekins <151791982+hmeekins@users.noreply.github.com>
2026-06-26 03:43:22 -0400

    Set up EF Core database context

---
+++ LearnapticAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Learnaptic.Api.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Learnaptic.Api.Models;
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<StudyGuide> StudyGuides { get; set; }
+        public DbSet<Flashcard> Flashcards { get; set; }
+        public DbSet<Concept> Concepts { get; set; }
+        public DbSet<KeyPoint> KeyPoints { get; set; }
+    }
+}
