99a3e0e3ee9ba5e222c2b5ccf25e615543e173fa
hmeekins <151791982+hmeekins@users.noreply.github.com>
2026-06-29 03:43:27 -0400

    Created CreateStudyGuideDto.cs

---
+++ LearnapticAPI/Dtos/CreateStudyGuideDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learnaptic.Api.Dtos
+{
+    public class CreateStudyGuideDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [StringLength(40, MinimumLength = 1)]
+        public string? Subject { get; set; }
+    }
+}
