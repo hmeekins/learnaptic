5d8d169fe49e729003df3fa6598895c1b8329abd
hmeekins <151791982+hmeekins@users.noreply.github.com>
2026-07-14 04:00:36 -0400

    Created GetStudySetListDTO and added GET endpoint to controller

--- Learnaptic.Api/Controllers/StudySetsController.cs
+++ Learnaptic.Api/Controllers/StudySetsController.cs
@@ -1,6 +1,8 @@
 ﻿using Learnaptic.Api.Data;
+using Learnaptic.Api.Dtos.StudySetDtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Learnaptic.Api.Controllers
 {
@@ -13,6 +15,21 @@ namespace Learnaptic.Api.Controllers
         public StudySetsController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStudySets()
+        {
+            var studySets = await _context.StudySets
+                .OrderByDescending(ss => ss.LastAccessedAt)
+                .Select(ss => new GetStudySetListDto
+                {
+                    Id = ss.Id,
+                    Title = ss.Title
+                })
+                .ToListAsync();
+
+            return Ok(studySets);
         }
     }
 }
---
+++ Learnaptic.Api/Dtos/StudySetDtos/GetStudySetListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Learnaptic.Api.Dtos.StudySetDtos
+{
+    public class GetStudySetListDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+    }
+}
