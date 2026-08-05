8d11e9b416b0102dce3003fe1b03317a13970fd2
hmeekins <151791982+hmeekins@users.noreply.github.com>
2026-08-05 03:35:07 -0400

    StudyGuide page now displays real data

---
+++ Learnaptic.Client/src/components/ConceptCard.tsx
@@ -0,0 +1,16 @@
+import type { Concept } from "../types/Concepts/Concept";
+
+type ConceptCardProps = {
+  concept: Concept;
+};
+
+function ConceptCard({ concept }: ConceptCardProps) {
+  return (
+    <section id={`concept-${concept.id}`}>
+      <h2>{concept.title}</h2>
+      <p>{concept.content}</p>
+    </section>
+  );
+}
+
+export default ConceptCard;
--- Learnaptic.Client/src/pages/StudyGuidePage.tsx
+++ Learnaptic.Client/src/pages/StudyGuidePage.tsx
@@ -1,20 +1,50 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import type { StudyGuideDetail } from "../types/StudyGuides/StudyGuideDetail";
+import type { Concept } from "../types/Concepts/Concept";
 import NavBar from "../components/NavBar";
 import ConceptTableOfContents from "../components/ConceptTableOfContents";
+import ConceptCard from "../components/ConceptCard";
 
 function StudyGuidePage() {
+  const { id } = useParams<{ id: string }>();
+  const [studyGuide, setStudyGuide] = useState<StudyGuideDetail | null>(null);
+  useEffect(() => {
+    async function loadStudyGuide() {
+      const response = await fetch(
+        `https://localhost:7057/api/study-guides/${id}`
+      );
+      const data: StudyGuideDetail = await response.json();
+      setStudyGuide(data);
+    }
+    loadStudyGuide();
+  }, []);
+
+  if (studyGuide === null) {
+    return (
+      <>
+        <NavBar />
+        <main>
+          <p>Loading study guide...</p>
+        </main>
+      </>
+    );
+  }
   return (
     <>
       <NavBar />
       <main>
         <header>
-          <h1>Biology Exam Review</h1>
-          <p>Biology • Updated yesterday</p>
+          <h1>{studyGuide?.title || ""}</h1>
+          <p>{studyGuide?.subject || ""}</p>
         </header>
 
-        <ConceptTableOfContents concepts={[]} />
+        <ConceptTableOfContents concepts={studyGuide?.concepts || []} />
 
         <section>
-          <p>Concept content goes here.</p>
+          {studyGuide?.concepts.map((concept: Concept) => (
+            <ConceptCard key={concept.id} concept={concept} />
+          ))}
         </section>
       </main>
     </>
