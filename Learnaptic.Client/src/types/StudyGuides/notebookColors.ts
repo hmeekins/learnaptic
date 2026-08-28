472add74e2b6d10958ad0fb8e832d0c016282d95
hmeekins <151791982+hmeekins@users.noreply.github.com>
2026-08-28 14:57:05 -0400

    Added list of notebook colors

---
+++ Learnaptic.Client/src/types/StudyGuides/notebookColors.ts
@@ -0,0 +1,30 @@
+const notebookColors = {
+  yellow: {
+    base: "#facc15",
+    dark: "#ca8a04",
+  },
+  red: {
+    base: "#ef4444",
+    dark: "#b91c1c",
+  },
+  green: {
+    base: "#22c55e",
+    dark: "#15803d",
+  },
+  blue: {
+    base: "#3b82f6",
+    dark: "#1d4ed8",
+  },
+  purple: {
+    base: "#a855f7",
+    dark: "#7e22ce",
+  },
+  pink: {
+    base: "#ec4899",
+    dark: "#be185d",
+  },
+  teal: {
+    base: "#14b8a6",
+    dark: "#0f766e",
+  },
+} as const;
